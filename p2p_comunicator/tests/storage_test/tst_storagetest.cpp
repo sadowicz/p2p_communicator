bd0e0ba17fb3e31b5c3367735265aebd003f4fb0
Michał Marczak <michal.m8833@gmail.com>
2021-01-25 17:40:37 +0100

    contactExists() test

--- p2p_comunicator/tests/storage_test/tst_storagetest.cpp
+++ p2p_comunicator/tests/storage_test/tst_storagetest.cpp
@@ -21,12 +21,58 @@ private slots:
         catch(std::out_of_range&) {}
 
         auto contacts = storage.getContacts();
+
         QCOMPARE(1, contacts.size());
+
         QCOMPARE("name", contacts["127.0.0.1"].getName());
+        QCOMPARE("127.0.0.1", contacts["127.0.0.1"].getAddress());
         QCOMPARE(1234, contacts["127.0.0.1"].getPort());
 
+        try {
+            storage.addContact(Contact{"other name", "22.77.34.12", 3412});
+        }
+        catch(std::out_of_range&) {}
+
+        contacts = storage.getContacts();
+
+        QCOMPARE(2, contacts.size());
+
+        QCOMPARE("name", contacts["127.0.0.1"].getName());
+        QCOMPARE("127.0.0.1", contacts["127.0.0.1"].getAddress());
+        QCOMPARE(1234, contacts["127.0.0.1"].getPort());
+
+        QCOMPARE("other name", contacts["22.77.34.12"].getName());
+        QCOMPARE("22.77.34.12", contacts["22.77.34.12"].getAddress());
+        QCOMPARE(3412, contacts["22.77.34.12"].getPort());
     }
 
+    void contactExists() {
+        Storage storage{};
+
+        QCOMPARE(0, storage.getContacts().size());
+
+        std::string address = "37.21.90.17";
+        std::string address2 = "10.20.30.73";
+
+        QCOMPARE(false, storage.contactExists(address));
+        QCOMPARE(false, storage.contactExists(address2));
+
+        try {
+            storage.addContact(Contact{"name", address, 1234});
+        }
+        catch(std::out_of_range&) {}
+
+        QCOMPARE(true, storage.contactExists(address));
+        QCOMPARE(false, storage.contactExists(address2));
+
+        try {
+            storage.addContact(Contact{"name", address2, 1234});
+        }
+        catch(std::out_of_range&) {}
+
+        QCOMPARE(true, storage.contactExists(address));
+        QCOMPARE(true, storage.contactExists(address2));
+    }
 };
 
 QTEST_APPLESS_MAIN(StorageTest)
