3a01afc1e4fe7aa82873bbe1ad1ca4f00ec5c110
Piotr Sniezek <sniegu703@gmail.com>
2021-02-04 18:31:00 +0100

    Remove file if is sent

--- p2p_comunicator/app/mainwindow.cpp
+++ p2p_comunicator/app/mainwindow.cpp
@@ -345,6 +345,7 @@ void MainWindow::on_pbSend_clicked() {
         Contact* contact = Storage::storage().getContact(activeContact);
         contactController->sendMessage(contact->getAddress(), ui->teSend->toPlainText().toStdString());
         ui->teSend->clear();    // clear msg text edit after sending
+        removeFile();
     }
 }
 
